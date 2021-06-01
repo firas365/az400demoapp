b13db0216cd9a951a696ff5e98b17bab615cf003
Firas <firas.mdimagh@outlook.com>
2021-06-01 13:16:00 +0200

    Add files via upload

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace az400demo.app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
